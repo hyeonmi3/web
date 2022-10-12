0f6ef59a9a5ac51b1ad34b35be3edfbfade2905c
hyeonmi3 <cc146@daum.net>
2022-10-12 11:02:56 +0900

    2학년 2학기 자바 수업

--- src/main/java/study/spring/web/controller/HomeController.java
+++ src/main/java/study/spring/web/controller/HomeController.java
@@ -1,16 +1,32 @@
 package study.spring.web.controller;
 
 // Conmponent의 자식 Controller
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import study.spring.web.domain.Member;
+import study.spring.web.domain.NameBookPost;
 import study.spring.web.domain.Person;
+import study.spring.web.service.MemberService;
+import study.spring.web.service.MemberServiceImpl;
+import study.spring.web.service.NameBookService;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private MemberService memberService;
+
+    private NameBookService nameBookService;
+
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     // RequestMapping /hello를 요청하면 밑에를 실행
     // method를 넣어서 POST로만 들어올수 있게 함
     // @RequestMapping(value = "/hello", method = RequestMethod.POST)
@@ -51,5 +67,61 @@ public class HomeController {
     public String add(@RequestParam("value1") int value1,
                       @RequestParam("value2") int value2) {
         return String.format("%d + %d = %d", value1, value2, value1 + value2);
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+    @PostMapping("/signin-result")
+    public ModelAndView signinResult(
+            @RequestParam("id") String id
+            , @RequestParam("name") String name
+            , @RequestParam("city") String city
+    ) {
+        ModelAndView mv = new ModelAndView("signin-result");
+
+        /* 데이터베이스에 아이디, 이름, 도시를 저장한다. */
+
+        Member member = new Member();
+        member.setId(id);
+        member.setName(name);
+        member.setCity(city);
+
+        member = memberService.register(member);
+
+        mv.addObject("member", member);
+
+        return mv;
+    }
+
+    @GetMapping("/namebook/write")
+    public String nameBook() {
+        return "namebook/write";
+    }
+
+    @PostMapping("/namebook/write-save")
+    public String namebookAdd(
+            @RequestParam("writer") String writer
+            , @RequestParam("content") String content
+    ) {
+        NameBookPost post = new NameBookPost();
+        post.setWriter(writer);
+        post.setContent(content);
+
+        nameBookService.add(post);
+
+        return "namebook/write-save";
+    }
+
+    @GetMapping("/namebook/list")
+    public ModelAndView nameBookList() {
+        List<NameBookPost> list = nameBookService.list();
+
+        ModelAndView mv = new ModelAndView("namebook/list");
+        mv.addObject("list", list);
+
+        return mv;
     }
 }
---
+++ src/main/java/study/spring/web/service/NameBookServiceImpl.java
@@ -0,0 +1,52 @@
+package study.spring.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import study.spring.web.domain.NameBookPost;
+import study.spring.web.Entity.NameBookPostEntity;
+import study.spring.web.repository.NameBookRepository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service(value = "nameBookService")
+public class NameBookServiceImpl implements NameBookService {
+    private NameBookRepository nameBookRepository;
+
+    @Autowired
+    public void setNameBookRepository(NameBookRepository nameBookRepository) {
+        this.nameBookRepository = nameBookRepository;
+    }
+
+    @Override
+    public NameBookPost add(NameBookPost post) {
+        NameBookPostEntity entity = new NameBookPostEntity();
+        entity.setWriter(post.getWriter());
+        entity.setContent(post.getContent());
+        entity.setRegisterTime(new Date());
+
+        nameBookRepository.save(entity);
+
+        post.setIdx(entity.getIdx());
+
+        return post;
+    }
+
+    @Override
+    public List<NameBookPost> list() {
+        List<NameBookPostEntity> entities = nameBookRepository.findAll();
+
+        List<NameBookPost> result = new ArrayList<>();
+        for (NameBookPostEntity item : entities) {
+            NameBookPost post = new NameBookPost();
+            post.setIdx(item.getIdx());
+            post.setWriter(item.getWriter());
+            post.setContent(item.getContent());
+
+            result.add(post);
+        }
+
+        return result;
+    }
+}
